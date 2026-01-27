6be1fadb868576c9f133e3db2db6535e898ab729
Нікіта Різник <nikita.rezzz@gmail.com>
2026-01-27 09:09:44 +0200

    observations about jvm memory

--- Pr3_Riznyk/src/main/java/MemoryClass.java
+++ Pr3_Riznyk/src/main/java/MemoryClass.java
@@ -4,12 +4,24 @@ public class MemoryClass {
         int num1 = num;
         String person = "Alice";
         String person1 = person;
+        int[] arr = {1, 2, 3};
+        int[] arr1 = arr;
 
         System.out.println(num);
         System.out.println(num1);
 
         System.out.println(person);
         System.out.println(person1);
+
+        for(int n : arr) {
+            System.out.print(n + " ");
+        }
+        for(int n : arr1) {
+            System.out.print(n + " ");
+        }
+
+        //Objects are stored in heap memory and referenced in stack memory.
+        //Primitives are stored directly in stack memory.
     }
 
     public static void main(String[] args) {
